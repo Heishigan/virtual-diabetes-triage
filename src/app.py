9bdf7d6921ba6c3ab0316ec5a44924562426274d
agatazz <a.z.zabuska@gmail.com>
2025-10-16 02:31:48 +0200

    api

---
+++ src/app.py
@@ -0,0 +1,36 @@
+# app/main.py
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, Field
+from src.model import get_model, MODEL_VERSION
+
+app = FastAPI(
+    title="Virtual Diabetese Triage API",
+    version=MODEL_VERSION,
+)
+
+class DataStructure(BaseModel):
+    age: float = Field(..., description="Age in years", ge = 0, le = 120)
+    sex: float = Field(..., description="Sex indicator (1 = female, 2 = male)", ge = 1, le = 2) # kinda guessing tbh since the dataset doesn't specify, no info on it from the original paper the data comes from as well
+    bmi: float = Field(..., description="Body Mass Index", gt = 0, le = 70)
+    bp: float = Field(..., description="Mean arterial blood pressure",gt = 0)
+    s1: float = Field(..., description="tc (T-Cell count)", gt = 0)
+    s2: float = Field(..., description="ldl cholesterol", gt = 0)
+    s3: float = Field(..., description="hdl cholesterol", gt = 0)
+    s4: float = Field(..., description="tch (thyroid hormone)", gt = 0)
+    s5: float = Field(..., description="ltg (log of serum triglycerides)", gt = 0)
+    s6: float = Field(..., description="glu (blood glucose)", gt = 0)
+
+@app.post("/predict")
+def predict(features: DataStructure):
+    try:
+        model = get_model()
+        prediction = model.predict(features.dict())
+        return {"prediction": prediction, "model_version": MODEL_VERSION}
+    except ValueError as e:
+        raise HTTPException(status_code = 422, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code = 400, detail=str(e))
+
+@app.get("/health", status_code=status.HTTP_200_OK)
+def health():
+    return {"status": "ok", "model_version": MODEL_VERSION}
---
+++ src/model.py
@@ -0,0 +1,48 @@
+import os
+import joblib
+# import numpy as np
+import pandas as pd
+import json
+
+ARTIFACTS_DIR = "artifacts"
+MODELS_DIR = os.path.join(ARTIFACTS_DIR, "models")
+METRICS_DIR = os.path.join(ARTIFACTS_DIR, "metrics")
+
+MODEL_PATH = os.path.join(MODELS_DIR, "model.joblib")
+SCALER_PATH = os.path.join(MODELS_DIR, "scaler.joblib")
+METRICS_PATH = os.path.join(METRICS_DIR, "metrics.json")
+try:
+    with open(METRICS_PATH, 'r') as f:
+        metrics = json.load(f)
+    MODEL_VERSION = metrics.get("model_version", "version_not_found")
+except FileNotFoundError:
+    MODEL_VERSION = "v0.0_uninitialized" 
+except json.JSONDecodeError:
+    MODEL_VERSION = "v0.0_error"
+
+FEATURE_NAMES = ['age','sex','bmi','bp','s1','s2','s3','s4','s5','s6']
+
+class DataModel:
+    def __init__(self, model_path = MODEL_PATH, scaler_path = SCALER_PATH):
+        if not os.path.exists(model_path) or not os.path.exists(scaler_path):
+            raise FileNotFoundError("Model not found. Please train the model first.")
+        self.model = joblib.load(model_path)
+        self.scaler = joblib.load(scaler_path)
+
+    def predict(self, features: dict):
+        missing = [f for f in FEATURE_NAMES if f not in features]
+        if missing:
+            raise ValueError(f"Missing features: {missing}")
+
+        X_input = pd.DataFrame([features[f] for f in FEATURE_NAMES], index=FEATURE_NAMES).T
+        X_scaled = self.scaler.transform(X_input)
+        y_pred = self.model.predict(X_scaled)
+        # print(X_scaled) # scales seem off
+        return float(y_pred[0])
+
+_model_instance = None
+def get_model():
+    global _model_instance
+    if _model_instance is None:
+        _model_instance = DataModel()
+    return _model_instance
